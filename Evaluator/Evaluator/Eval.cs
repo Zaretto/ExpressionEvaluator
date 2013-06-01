c3335cd015eefb1954a59c9ed9524ddb3c3cab1a
Richard Harrison <rjh@zaretto.com>
2013-06-01 12:58:07 +0200

    ported from C - initial version in C#

---
+++ Evaluator/Evaluator/Eval.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evaluator
+{
+    class Eval
+    {
+        /*		Expression evaluator for symon. Will take expression and  
+         *	return either a double, or a 4 byte unsigned integer (for hex or   
+         *      ASCII input of values                                            
+         * 
+         *	Author:	R.J.Harrison		Date	15-December-1987          
+        */
+
+        Stack<double> stack;
+
+        int txtptr = 0;
+        static double fac = 0;
+        string instring;
+
+        public double eval(String input)
+        {
+            instring = input;
+            txtptr = 0;
+            stack = new Stack<double>();
+            
+            level1('\0');
+            return fac;
+            //            return type;
+        }
+
+        char level1(char sign)
+        {
+            do
+            {
+                sign = level2(sign);
+            }
+            while (sign > 0);
+            return sign;
+        }
+
+        char level2(char sign)
+        {
+            return level3(sign);
+        }
+
+        char level3(char sign)
+        {
+            if (sign == '+')
+            {
+                stack.Push(fac);
+                sign = level4(sign);
+                fac = stack.Pop() + fac;
+                return sign;
+            }
+            if (sign == '-')
+            {
+                stack.Push(fac);
+                sign = level4(sign);
+                fac = stack.Pop() - fac;
+                return sign;
+            }
+            return level4(sign);
+        }
+
+        char level4(char sign)
+        {
+            if (sign == '*')
+            {
+                stack.Push(fac);
+                sign = level5(sign);
+                fac = stack.Pop() * fac;
+                return sign;
+            }
+            if (sign == '/')
+            {
+                stack.Push(fac);
+                sign = level5(sign);
+                fac = stack.Pop() / fac;
+                return sign;
+            }
+            return level5(sign);
+        }
+
+        char level5(char sign)
+        {
+            if (sign == '^')
+            {
+                stack.Push(fac);
+                sign = level6(sign);
+                fac = Math.Pow(stack.Pop(), fac);
+                return sign;
+            }
+            return level6(sign);
+        }
+
+        char level6(char sign)
+        {
+            /*
+             * see if end of bracketed expression
+             */
+            if (sign == ')')
+                return '\0';
+
+            fac = 0;
+
+            sign = instring[txtptr];
+
+            /*
+             * handle variables
+             */
+            if (Char.IsLetter(sign))
+            {
+                int end = txtptr;
+                while (end < instring.Length && (instring[end] == '.' || Char.IsLetterOrDigit(instring[end])))
+                {
+                    end++;
+                }
+                var sv = instring.Substring(txtptr, end - txtptr);
+                txtptr = end;
+                Console.WriteLine("Found var {0}", sv);
+                fac = 0;
+            }
+            else
+            {
+                switch (sign)
+                {
+                    //case '-':
+                    //    txtptr++;
+                    //    negate = !negate;
+                    //    goto restart;
+                    //    break;
+
+                    //case '&' :
+                    //    txtptr++;
+                    //    strg = chex();
+                    //    break;
+
+                    //case '+':
+                    //    txtptr++; // leading + is a no-op.
+                    //    goto restart;
+                    //    //fac = cint();
+                    //break;
+
+                    case '(':
+                        txtptr++;
+                        level1(instring[txtptr]);
+                        break;
+
+                    default:
+                        {
+                            var end = txtptr;
+                            bool can_negate = true;
+                            while (end < instring.Length
+                                   && (Char.IsDigit(instring[end]) || instring[end] == '.' || (can_negate && instring[end] == '-')))
+                            {
+                                can_negate = false;
+                                end++;
+                            }
+                            var sv = instring.Substring(txtptr, end - txtptr);
+                            txtptr = end;
+                            fac = Double.Parse(sv);
+                            //          fac = cint();
+                            break;
+                        }
+                }
+            }
+            if (txtptr < instring.Length)
+                sign = instring [txtptr++];
+            else
+            {
+                sign = '\0';
+            }
+            return sign;
+        }
+    }
+}
---
+++ Evaluator/Evaluator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evaluator
+{
+    class Program
+    {
+        static Eval x = new Eval();
+        static void teval(string v, int expected)
+        {
+            var res = x.eval(v);
+            System.Console.WriteLine("{0} = {1}", v, res);
+            if (res != expected)
+                System.Console.WriteLine(" Expected {0}", expected);
+
+        }
+
+        static void Main(string[] args)
+        {
+            teval("ENV.MAIN+ENV.SECONDAY", 0);
+            teval("-10+20", 10);
+            teval("-10--30", 20);
+            teval("-10-20", -30);
+            teval("10-20+30", 20);
+            teval("100+200", 300);
+            teval("(10-20)+30", 20);
+            teval("10-(20+30)", -40);
+            teval("2*3+4", 10);
+            teval("2*6/3", 4);
+            teval("2^2", 4);
+        }
+    }
+}
